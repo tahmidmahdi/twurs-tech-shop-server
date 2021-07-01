9a3d100b7aa65a4d623a08c78d1537d91272bd9f
tahmidmahdi <tahmid5550@gmail.com>
2021-07-01 23:20:17 +0600

    api for single product

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
 const app = express()
+
 app.use(cors())
 app.use(bodyParser.json())
+const { ObjectId } = require('bson');
 const { MongoClient } = require('mongodb');
 
 
@@ -21,13 +22,31 @@ client.connect(err => {
   console.log('databaseConnected')
 
 
-
+  //to add products to database
   app.post('/addProduct', (req, res) => {
     console.log(req.body)
     productsCollection.insertOne(req.body)
-    .then(console.log('added Successfully'))
+      .then(console.log('added Successfully'))
+  })
+  
+
+  app.get('/allProducts', (req, res) => {
+      productsCollection.find({})
+      .toArray((err, collections) => {
+          res.send(collections)  
+      })
   })
 
+
+
+  app.get('/getProductData/:data', (req, res) => {
+      console.log(req.params.data)
+      productsCollection.find({_id : ObjectId(req.params.data)})
+      .toArray((err, collections)=> {
+        console.log(collections)
+        res.send(collections)
+      })
+  })
 
 
 });
